588854160ed4a4c57ca2e0b6c560c290d7afda9e
Emanuel Silva <emanuel.silva@grupofm.local>
2023-04-27 12:21:18 -0300

    adicionar funcionalidades de verificar as agendas no dia, semana e mes

--- src/components/Calendario/Calendario.vue
+++ src/components/Calendario/Calendario.vue
@@ -7,6 +7,10 @@
   import interactionPlugin from '@fullcalendar/interaction'
   import listPlugin from '@fullcalendar/list'
   import timeGridPlugin from '@fullcalendar/timegrid'
+  import esLocale from '@fullcalendar/core/locales/pt-br';
+  import 'bootstrap/dist/css/bootstrap.css';
+  import 'bootstrap-icons/font/bootstrap-icons.css';
+  import bootstrap5Plugin from '@fullcalendar/bootstrap5';
   
   export default {
     components: {
@@ -15,20 +19,56 @@
     data() {
       return {
         opcoesCalendario: {
-          plugins: [ dayGridPlugin, interactionPlugin, listPlugin, timeGridPlugin ],
+          plugins: [ dayGridPlugin, interactionPlugin, listPlugin, timeGridPlugin, bootstrap5Plugin ],
           initialView: 'dayGridMonth',
-          // headerToolbar: {
-          //     start: '1', // will normally be on the left. if RTL, will be on the right
-          //     center: '2',
-          //     end: '3' // will normally be on the right. if RTL, will be on the left
-          // },
+          headerToolbar: {
+            left: 'prev,next',
+            center: 'title',
+            right: 'timeGridDay, dayGridWeek, dayGridMonth, listDay' // user can switch between the two
+          },
           selectable: true,
           editable: true,
+          locale: esLocale,
+          // select: this.selecionaData,
+          eventClick: this.eventClick,
+          themeSystem: 'bootstrap5',
+          height: 650,
+          events: [
+          {
+            title: 'Meeting',
+            start: '2019-08-12T14:30:00',
+            extendedProps: {
+              status: 'done'
+            }
+          },
+          {
+            title: 'Birthday Party',
+            start: '2019-08-13T07:00:00',
+            backgroundColor: 'green',
+            borderColor: 'green'
+          }
+          ]
         }
       }
     },
     methods: {
-  
+      selecionaData(arg) {
+        id.value = id.value += 1
+
+        const cal = arg.view.calendar
+        cal.unselect()
+        cal.addEvent({
+          id: `${id.value}`,
+          title: 'Novo agendamento',
+          start: arg.start,
+          end: arg.end,
+          allDay: true
+        })
+      },
+
+      eventClick(arg) {
+        alert(arg.event.title)
+      }
     }
   }
   </script>
